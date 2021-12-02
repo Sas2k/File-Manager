c717bafb658c6f55ee3fc6da9033832b7b8c71bb
Sasen Perera <sasen.learnings@gmail.com>
2021-12-02 08:26:19 +0530

    3.0 Update Commit

---
+++ File_Manager.py
@@ -0,0 +1,302 @@
+import argparse
+import os
+import sys
+import time
+
+import blessed
+
+parser = argparse.ArgumentParser()
+
+global Path
+Path = os.getcwd() + '\\'
+history = []
+ani = '[==============]'
+term = blessed.Terminal()
+run = True
+x = 0
+help_message = """
+#=============================================#
+#+++++++++++++++++++++HELP++++++++++++++++++++#
+#=============================================#
+#  commands | description                     #
+#1.edit file | edits a file                   #
+#2.create file | creates a file               ##
+#3.read file | reads a file and display it here#
+#4.delete file | deletes a file               ##
+#5.help | displays this message               #
+#6.exit | To exit the program                 ###############
+#7.create folder | creates a folder in the current directory#
+#8.delete folder | deletes a folder                         #
+#9.list files | list the files of a path                    #
+#10.list paths | list the paths of a path                   #
+#NOTE:Don't do the list path command in the C:\ directory   #
+#11.set path | sets the  path for other commands            #################################
+#12.show path | shows the current path your working on                                      #
+#Note:if the path is not set the default path is the current place where you downloaded this#
+#13.show history | Shows all the commands you used                                          #
+#14.settings | Just the settings                                                            #
+#===========================================================================================#
+"""
+####FM Script runner####
+"""def runscript():
+    script_name = input('[Script Name]')
+    c = 0
+    with open(f'{Path}{script_name}', 'r') as sc:
+        global file_to_open
+        global scr
+        lines = sc.readlines()
+        lines = [line.rstrip() for line in lines]
+        for r in range(0, len(lines)):
+            a = lines[r]
+            b = lines[r+1]
+            if a == 'create file':
+                if b[0:6] == 'name = ':
+                    ghfile = str(b[7:])
+                with open(f'{Path}{ghfile}', 'w+') as cf:
+                    cf.close()
+                scr = True
+            elif a == 'edit file':
+                if b[0:6] == 'name = ':
+                    ehfile = str(b[7:])
+                with open(f'{Path}{ehfile}', 'w+') as cf:
+                    cf.close()
+                scr = True
+            elif a[0:6] == 'name = ':
+                pass
+
+    if scr == True:
+        print('Script has been executed succesfully')
+    else:
+        print('The script has an error')"""
+####CLI Commands####
+parser.add_argument("-list", help="lists the files in the current directory",
+                    action="store_true", required=False)
+parser.add_argument("-curname", help="Displays the current directory name",
+                    action="store_true", required=False)
+parser.add_argument("-curpath", help="Displays the current directory path",
+                    action="store_true", required=False)
+parser.add_argument("-cfile", help="creates a file",
+                    action="store_true", required=False)
+parser.add_argument("-dfile", help="Deletes a file",
+                    action="store_true", required=False)
+
+args = parser.parse_args()
+
+if args.list:
+    files = os.listdir(os.curdir)
+    for f in files:
+        print(f)
+    run = False
+
+if args.curname:
+    folder_name = os.getcwd()
+    print(os.path.basename(folder_name))
+    run = False
+
+if args.curpath:
+    dir_path = os.getcwd()
+    print(dir_path)
+    run = False
+
+if args.cfile:
+    file_name = input('[Enter Name]:')
+    with open(file_name, 'w+') as d:
+        print('file created')
+        d.close
+    run = False
+
+if args.dfile:
+    file_name = input('[Enter Name]:')
+    if os.path.exists(file_name):
+        os.remove(file_name)
+    else:
+        print(term.red('[ERROR]:No File Found with that name'))
+    run = False
+###############################
+def delete_last_line():
+    sys.stdout.write('\x1b[1A')
+    sys.stdout.write('\x1b[2K')
+def clear():
+    # for windows
+    if os.name == 'nt':
+        _ = os.system('cls')
+    # for mac and linux(here, os.name is 'posix')
+    if os.name != 'nt':
+        _ = os.system('clear')
+###########Settings############
+color = term.yellow
+warning_color = term.red
+edit_color = term.blue
+###############################
+if run == True:
+    print(term.green('[:Made by Sasen Perera 2021:]\n[For help type "help"]\n[Version 3.0.0]'))
+while run:
+    x += 1
+    inp = input(color(f'[{x}]:'))
+    if inp == 'exit':
+        print(warning_color('[]Are you sure?'))
+        ex = input(warning_color('[y/n]:'))
+        if ex == 'y':
+            history = []
+            run = False
+        elif ex == 'n':
+            pass
+    
+    elif inp == 'set path':
+        history.append('set path')
+        Path = input('[Enter Path]:')
+
+    elif inp == 'show path':
+        history.append('show path')
+        print(Path)
+
+    elif inp == 'settings':
+        history.append('settings')
+        print('[color for prompts ex.[0]]')
+        cc = input('[red, green, yellow[default], blue]:')
+        match cc:
+            case 'red':
+                color = term.red
+            case 'green':
+                color = term.green
+            case 'yellow':
+                color = term.yellow
+            case 'blue':
+                color = term.blue
+            case None:
+                color = term.yellow
+        print('[color for warnings]')
+        wc = input('[red[default], green, yellow, blue]:')
+        match wc:
+            case None:
+                warning_color = term.red
+            case 'red':
+                warning_color = term.red
+            case 'green':
+                warning_color = term.green
+            case 'yellow':
+                warning_color = term.yellow
+            case 'blue':
+                warning_color = term.blue
+        print('[color for edits ex. [>>]]')
+        ec = input('[red, green, yellow, blue[default]]:')
+        match ec:
+            case None:
+                edit_color = term.blue
+            case 'red':
+                edit_color = term.red
+            case 'green':
+                edit_color = term.green
+            case 'yellow':
+                edit_color = term.yellow
+            case 'blue':
+                edit_color = term.blue
+    elif inp == 'create file':
+        history.append('create file')
+        file_name = input('[enter name]:')
+        with open(f'{Path}{file_name}', 'w+') as g:
+            for y in range(len(ani)+1):
+                print(ani[0:y])
+                time.sleep(1)
+                delete_last_line()
+            print('[]file created')
+            print('[]To write this file write "write file" command and write the name')
+            g.close
+
+    elif inp == 'edit file':
+        history.append('edit file')
+        file_name = input('[enter name]:')
+        with open(f'{Path}{file_name}', 'w', encoding="utf-8") as f:
+            edit = True
+            while edit:
+                i_e = input(warning_color('[Status[stop,resume]]:'))
+                if i_e == 'stop':
+                    f.close()
+                    edit = False
+                elif i_e == 'resume':
+                    e_i = input(edit_color('[>>]:'))
+                    if e_i == '<n>':
+                        f.write('\n')
+                    else:
+                        f.write(e_i)
+
+    elif inp == 'read file':
+        history.append('read file')
+        file_name = input('[enter name]:')
+        with open(f'{Path}{file_name}', 'r') as h:
+            a = h.read()
+            print(f'{a}')
+            h.close()
+
+    elif inp == 'delete file':
+        file = input('[enter name]:')
+        filepath = f'{Path}{file}'
+        for z in range(len(ani)+1):
+            print(ani[0:z])
+            time.sleep(1)
+            delete_last_line()
+        os.remove(filepath)
+        print('[ ]File Deleted')
+        history.append('delete file')
+
+    elif inp == 'help':
+        print(help_message)
+    
+    elif inp == 'create folder':
+        folder_name = input('[enter name]:')
+        folder_path = f'{Path}{folder_name}'
+        for a in range(len(ani)+1):
+            print(ani[0:a])
+            time.sleep(1)
+            delete_last_line()
+        os.makedirs(folder_path)
+        print(term.green("Folder "+folder_name+" Created"))
+        history.append('create folder')
+
+    elif inp == 'delete folder':
+        folder_name = input('[enter name]:')
+        folder_path = f'{Path}{folder_name}'
+        if len(os.listdir(folder_path)) == 0:
+            for b in range(len(ani)+1):
+                print(ani[0:b])
+                time.sleep(1)
+                delete_last_line()
+            os.rmdir()
+            history.append('delete folder')
+        else:
+            print(term.red('Folder not empty'))
+
+    elif inp == 'list files':
+        history.append('list files')
+        files = os.listdir(Path)
+
+        for f in files:
+            print(f)
+
+    elif inp == 'list paths':
+        history.append('list paths')
+        for root, directories, files in os.walk(Path, topdown=False):
+            for name in files:
+                print(os.path.join(root, name))
+            for name in directories:
+                print(os.path.join(root, name))
+    
+    elif inp == 'show history':
+        history.append('show history')
+        for k in range(0,len(history)):
+            print(f'({k+1}){history[k]}')
+
+    # elif inp == 'run script':
+        # runscript()
+
+    elif inp == '':
+        """Nothing will happen"""
+
+    elif inp[0] == "'" and inp[-1] == "'" or inp[0] == '"' and inp[-1] == '"':
+        print(inp[1:-1])
+
+    elif inp == 'clear':
+        clear()
+
+    else:
+        print(warning_color('[]incorrect command'))
---
+++ README.md
@@ -0,0 +1,109 @@
+# File Manager 📁
+Version 3.0.0
+
+A simple cli based File Manager
+## Documentation
+
+### commands
+
+|  command       |  description                   |
+|:----------     | :-------------                 |
+|`create file`   | creates a file                 | 
+|`delete file`   | deletes a file                 |
+|`read file`     | reads a file                   | 
+|`edit file`     | edits a file                   |
+|`help`          | displays the help message      |
+|`exit`          | exits the program              |
+|`create folder` | creates a folder               |
+|`delete foler`  | deletes a folder               | 
+|`list files`    | list the files in a directory  |
+|`list paths`    | list the paths in a directory  |
+|`set paths`     | sets the path for the manager  |
+|`show path`     | shows the path your working on |
+|`show history`  | shows the the commands you used|
+|`settings`      | settings
+
+NOTE:File_Manager's default path is the current place it id downloaded
+
+### Edit commands
+|  command       |  description                 |
+|:----------     | :-------------               |
+|`<n>`           | takes a new line             |
+
+### cli-commands [V2.0.0(and newer) Feature]
+#### How to use cli command
+##### Source Code
+Firstly change your directory in your cli to the place you<br>downloaded the file manager source code
+```cmd
+C:>Users/user>cd path/to/file
+C:path/to/file>Python -m 'File Manager.py' <put the flags here>
+```
+##### EXE
+Firstly read the installation section then come here;
+Hello Welcome back now that you have read the installation section <br>Now let's get started
+```cmd
+C:/Users/user>file_Manager <flag here>
+```
+
+|  flag/command       |  description                             |
+|:----------          | :-------------                           |
+|`-list`              | lists the files in the current directory |
+|`-curname`           | Displays the current directory name      |
+|`-curpath`           | Displays the current directory path      |
+|`-cfile`             | creates a file                           |
+|`-dfile`             | deletes a file                           |
+|`-h` or `-help`      | displays help                            |
+
+## Installation
+
+### Source Code
+
+click on the "Version x.x.x" Tag from releases from that it will be downloaded <br>
+then on your command line change the directory to the place where you downloaded it <br>
+Then install the requirements with the command below;
+
+CMD
+```cmd
+    pip install -r requirements.txt
+```
+
+Bash
+```bash
+    $ python -m pip install -r requirements.txt
+```
+Then run the file with this commmand;
+```cmd
+    python Main.py
+```
+Or;
+
+You can just unzip the file [if you haven't already done it at the start of the process]<br> and double click/run it with python😏
+
+### Windows
+
+The `File_Manager.exe` file can be found when you unzip source code [sorry 😅] or by going to the code section and click the File_Manager.exe file and<br>click downloaded<br>
+also you might want to add the folder where the .exe is intalled to path then;
+
+```cmd
+File_Manager -h
+```
+if you get the output of this;
+
+```cmd
+usage: File_Manager.exe [-h] [-list] [-curname] [-curpath] [-cfile] [-dfile]
+
+options:
+  -h, --help  show this help message and exit
+  -list       lists the files in the current directory
+  -curname    Displays the current directory name
+  -curpath    Displays the current directory path
+  -cfile      creates a file
+  -dfile      Deletes a file
+```
+Then you have correctly setted up the file manager.
+## Badges
+
+[![Run on Repl.it](https://repl.it/badge/github/Sas2k/File-Manager)](https://repl.it/github/Sas2k/File-Manager)
+
+[![Github Downloads (total)](https://img.shields.io/github/downloads/Sas2k/File-Manager/total.svg)]()
+
